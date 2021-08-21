d884e5e61c9f8f269e0d95f8ab07aef1deb8231d
tianxu <xu.tian@b-and-qchina.com>
2021-08-21 23:22:33 +0800

    fix jackson

--- worker-biz/src/main/java/com/apising/worker/config/WebConfig.java
+++ worker-biz/src/main/java/com/apising/worker/config/WebConfig.java
@@ -3,6 +3,7 @@ package com.apising.worker.config;
 import com.apising.worker.interceptor.LoginInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
@@ -18,14 +19,14 @@ public class WebConfig extends WebMvcConfigurerAdapter {
         super.addInterceptors(registry);
     }
 
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("null")
-//                .allowedMethods("POST","GET","OPTION","PUT","DELETE")
-//                .allowedHeaders("*")
-//                .maxAge(3600)
-//                .allowCredentials(true);
-//        super.addCorsMappings(registry);
-//    }
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("null")
+                .allowedMethods("POST","GET","OPTION","PUT","DELETE")
+                .allowedHeaders("*")
+                .maxAge(3600)
+                .allowCredentials(true);
+        super.addCorsMappings(registry);
+    }
 }
